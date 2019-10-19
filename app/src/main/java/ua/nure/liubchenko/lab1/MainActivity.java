c480d9f1354f3853d9248b5108694f7f501a6aed
Yuriy Lyubchenko <undeadspez@gmail.com>
2019-10-19 22:03:43 +0300

    Add proper navigation for back button

--- app/src/main/java/ua/nure/liubchenko/lab1/MainActivity.java
+++ app/src/main/java/ua/nure/liubchenko/lab1/MainActivity.java
@@ -3,19 +3,17 @@ package ua.nure.liubchenko.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.navigation.NavController;
-import androidx.navigation.NavGraph;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
 
 import ua.nure.liubchenko.lab1.databinding.MainActivityBinding;
-import ua.nure.liubchenko.lab1.ui.notelist.NoteListFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +25,18 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(
                 this, R.id.nav_host_fragment);
 
-        AppBarConfiguration appBarConfiguration =
+        appBarConfiguration =
                 new AppBarConfiguration.Builder(navController.getGraph()).build();
-
-        //Toolbar toolbar = findViewById(R.id.toolbar);
-
-        //NavigationUI.setupWithNavController(toolbar, navController);
 
         NavigationUI.setupActionBarWithNavController(
                 this, navController, appBarConfiguration);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(
+                this, R.id.nav_host_fragment);
+
+        return NavigationUI.navigateUp(navController, appBarConfiguration);
     }
 }
